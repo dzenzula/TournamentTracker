6cfad5260062160a490dcd0855f96615449da26a
Artem Dzenzura <dzenzura.artjom@gmail.com>
2022-02-01 17:46:42 +0200

    Clearing form

--- TrackerUi/CreatePrizeForm.Designer.cs
+++ TrackerUi/CreatePrizeForm.Designer.cs
@@ -95,6 +95,7 @@
             this.prizeAmountValue.Name = "prizeAmountValue";
             this.prizeAmountValue.Size = new System.Drawing.Size(183, 35);
             this.prizeAmountValue.TabIndex = 28;
+            this.prizeAmountValue.Text = "0";
             // 
             // prizeAmountLabel
             // 
@@ -113,6 +114,7 @@
             this.prizePercentageValue.Name = "prizePercentageValue";
             this.prizePercentageValue.Size = new System.Drawing.Size(183, 35);
             this.prizePercentageValue.TabIndex = 30;
+            this.prizePercentageValue.Text = "0";
             // 
             // prizePercentageLabel
             // 
--- TrackerUi/CreatePrizeForm.cs
+++ TrackerUi/CreatePrizeForm.cs
@@ -32,6 +32,11 @@ namespace TrackerUi
                 {
                     db.CreatePrize(model);
                 }
+
+                placeNameValue.Text = "";
+                placeNumberValue.Text = "";
+                prizeAmountValue.Text = "0";
+                prizePercentageValue.Text = "0";
             }
             else
             {
